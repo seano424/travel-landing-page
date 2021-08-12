9b54a04cf3a5f17127e9098e84994f0158536c75
Sean OReilly <soreilly424@gmail.com>
2021-08-12 17:26:33 -0500

    navbar relative added

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,6 +1,6 @@
 export default function Navbar({ children }) {
   return (
-    <nav className="fixed w-full py-4 z-50">
+    <nav className="fixed md:relative w-full py-4 z-50">
       <ul className="flex space-x-2 justify-between items-center">
         {children}
       </ul>
